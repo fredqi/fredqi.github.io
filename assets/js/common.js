935aebfdf56b39f326de4608789edfb1ff6daf0f
Fei Qi <fred.qi@ieee.org>
2020-08-17 16:05:48 +0800

    Refactored codes for the generation of publication list.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,5 @@
 
-function toggleBibTeX(parameter) {
+function toggleTextblock(parameter) {
     var x= document.getElementById(parameter);
     if (x.style.display === 'none') {
         x.style.display = 'block';
